834b5c69d759fb936dedb588b439c70a3dff6281
Gustavo Silva <gstv.silva@gmail.com>
2017-03-09 16:38:08 -0300

    segundo commit

---
+++ PrevisaoTempo.Core/WeatherService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Net;
+using System.Collections.Generic;
+
+namespace PrevisaoTempo.Core
+{
+    public class CityWeather
+    {
+        [JsonProperty("location")]
+        public string Location { get; set; }
+
+        [JsonProperty("temperature")]
+        public string Temperature { get; set; }
+
+        [JsonProperty("skytext")]
+        public string Skytext { get; set; }
+
+        [JsonProperty("humidity")]
+        public string Humidity { get; set; }
+
+        [JsonProperty("wind")]
+        public string Wind { get; set; }
+
+        [JsonProperty("date")]
+        public string Date { get; set; }
+
+        [JsonProperty("day")]
+        public string Day { get; set; }
+    }
+    
+
+    public class WeatherService
+	{
+        HttpClient httpClient;
+
+        public WeatherService(){
+            httpClient = new HttpClient();
+        }
+
+        public async Task<CityWeather> GetDataCity(string city) {
+            var jsonResponse = await httpClient.GetStringAsync("http://weathers.co/api.php?city="+city);
+            var parsedJson = JObject.Parse(jsonResponse)["data"].ToObject<CityWeather>();
+
+            return parsedJson;
+        }
+	}
+
+}
---
+++ PrevisaoTempo.Droid/MainActivity.cs
@@ -0,0 +1,56 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using PrevisaoTempo.Core;
+
+namespace PrevisaoTempo.Droid
+{
+    [Activity(Label = "PrevisaoTempo.Droid", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        WeatherService weatherService = new WeatherService();
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.Main);
+
+            var editTextCityName = FindViewById<EditText>(Resource.Id.editTextCityName);
+            var buttonSearch = FindViewById<Button>(Resource.Id.buttonSearch);
+
+            var textViewLocation = FindViewById<TextView>(Resource.Id.textViewLocation);
+            var textViewTemperature = FindViewById<TextView>(Resource.Id.textViewTemperature);
+            var textViewSkytext = FindViewById<TextView>(Resource.Id.textViewSkytext);
+            var textViewHumidity = FindViewById<TextView>(Resource.Id.textViewHumidity);
+            var textViewWind = FindViewById<TextView>(Resource.Id.textViewWind);
+            var textViewDate = FindViewById<TextView>(Resource.Id.textViewDate);
+            var textViewDay = FindViewById<TextView>(Resource.Id.textViewDay);
+
+
+            buttonSearch.Click += async (object sender, System.EventArgs e) => {
+
+                if (!editTextCityName.Text.Equals("")) {
+                    textViewLocation.Text = "Aguarde...";
+                    textViewTemperature.Text = "";
+                    textViewSkytext.Text = "";
+                    textViewHumidity.Text = "";
+                    textViewWind.Text = "";
+                    textViewDate.Text = "";
+                    textViewDay.Text = "";
+
+                    var result = await weatherService.GetDataCity(editTextCityName.Text);
+
+                    textViewLocation.Text = "Location: "+result.Location;
+                    textViewTemperature.Text = "Temperature: " + result.Temperature;
+                    textViewSkytext.Text = "Skytext: " + result.Skytext;
+                    textViewHumidity.Text = "Humidity: " + result.Humidity;
+                    textViewWind.Text = "Wind: " + result.Wind;
+                    textViewDate.Text = "Date: " + result.Date;
+                    textViewDay.Text = "Day: " + result.Day;
+                }
+            };
+        }
+    }
+}
+
